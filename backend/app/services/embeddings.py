67611ea3b7d1fd254d8a2a6b73a4bda252942bf6
Ramsha Arshad <arshadramsha04@gmail.com>
2026-05-02 02:27:48 +0530

    fixed memory issue with lazy loading

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,6 +1,14 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2',device='cpu')
+    return model
+
 
 def get_embedding(text):
+    model = get_model()
     return model.encode(text).tolist()
--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://ai-data-catalog.onrender.com"
 
 # -----------------------------
 # 🧠 SESSION STATE INIT
